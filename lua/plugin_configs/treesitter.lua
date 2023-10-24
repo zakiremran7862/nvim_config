fc9616d9151791fc4b46dcf40621fd901c5304a3
zakiremran7862 <zakiremran@gmail.com>
2023-10-24 23:28:09 +0600

    initial setup

---
+++ lua/plugin_configs/treesitter.lua
@@ -0,0 +1,13 @@
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { "html", "css", "typescript", "javascript" },
+
+  sync_install = false,
+
+  auto_install = false,
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
